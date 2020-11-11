c29e96dab92009c8ea3af378ef5e9f104bff726b
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2020-11-11 23:43:12 +0100

    Clase 025: scss: mixin & include / jquery

--- 19/js/app.js
+++ 19/js/app.js
@@ -5,7 +5,12 @@ $("#cuadrado").click(function () {
     $("#cuadrado").css("background", "coral")
 })
 
-// desaparece en 2 segundos al hacer clic en JQuery
+// desaparece en 3 segundos al hacer clic en JQuery
 $("#cuadrado").click(function () {
     $("#cuadrado").fadeOut(2000)
+})
+
+// añade una clase que da padding al hacer doble clic
+$("#cuadrado").dblclick(function () {
+    $("#cuadrado").addClass("cuadrado-crece")
 })
