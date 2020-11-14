65aa57d0c83a6656cf9717a6c2c01cd774ff0cfd
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2020-11-14 13:08:39 +0100

    Clase 026: jquery: slider

--- 20/js/app.js
+++ 20/js/app.js
@@ -1,9 +1,13 @@
 // añade clases para hacer un slider
 
 $(".arrow__right").click(function () {
-    $(".container__huge").addClass("container__huge-show2");
+    $(".container__huge").addClass("container__huge-show2")
+    $(".arrow__left").removeClass("arrow__left-hide")
+    $(".arrow__right").addClass("arrow__right-hide")
 })
 
 $(".arrow__left").click(function () {
-    $(".container__huge").removeClass("container__huge-show2");
+    $(".container__huge").removeClass("container__huge-show2")
+    $(".arrow__right").removeClass("arrow__right-hide")
+    $(".arrow__left").addClass("arrow__left-hide")
 })
