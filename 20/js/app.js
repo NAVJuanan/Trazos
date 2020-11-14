5223e1baed793f99402765ed8458c36c36c20dd7
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2020-11-14 15:34:34 +0100

    Clase 026: jquery: mobile menu

--- 20/js/app.js
+++ 20/js/app.js
@@ -1,13 +1,18 @@
 // añade clases para hacer un slider
-
 $(".arrow__right").click(function () {
-    $(".container__huge").addClass("container__huge-show2")
-    $(".arrow__left").removeClass("arrow__left-hide")
-    $(".arrow__right").addClass("arrow__right-hide")
+    $(".container__huge").addClass("container__huge--show2")
+    $(".arrow__left").removeClass("arrow__left--hide")
+    $(".arrow__right").addClass("arrow__right--hide")
 })
 
 $(".arrow__left").click(function () {
-    $(".container__huge").removeClass("container__huge-show2")
-    $(".arrow__right").removeClass("arrow__right-hide")
-    $(".arrow__left").addClass("arrow__left-hide")
+    $(".container__huge").removeClass("container__huge--show2")
+    $(".arrow__right").removeClass("arrow__right--hide")
+    $(".arrow__left").addClass("arrow__left--hide")
+})
+
+
+// añade clases para hacer un menú de móvil
+$(".header__icon").click(function () {
+    $(".header__nav").toggleClass("header__nav--show")
 })
