4cb3c3ef1d2523922b26b4ba276fe6d858348fdc
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2020-11-20 20:08:35 +0100

    Clase 030: scss: & / css: slider múltiple

--- 22/js/app.js
+++ 22/js/app.js
@@ -3,10 +3,48 @@ let pictureCounter = 1;
 // clic en flecha derecha -> aumenta
 $(".slider__arrow-right").click(function () {
 
+    pictureCounter++;
+    movePicture();
+})
+
+// clic en flecha izquierda -> disminuye
+$(".slider__arrow-left").click(function () {
+
+    pictureCounter--;
+    movePicture();
+})
+
+// clic en los puntos para avanzar o retroceder
+$(".slider__point:nth-of-type(1)").click(function () {
+
+    pictureCounter = 1;
+    movePicture();
+})
+
+// clic en los puntos para avanzar o retroceder
+$(".slider__point:nth-of-type(2)").click(function () {
+
+    pictureCounter = 2;
+    movePicture();
+})
+
+// clic en los puntos para avanzar o retroceder
+$(".slider__point:nth-of-type(3)").click(function () {
+
+    pictureCounter = 3;
+    movePicture();
+})
+
+
+// función para mover las imágenes
+function movePicture() {
+
     let translatePercentage = 0;
     const percentage = -33;
 
-    pictureCounter++;
+    if (pictureCounter === 0) {
+        pictureCounter = 1;
+    }
 
     if (pictureCounter === 4) {
         pictureCounter = 3;
@@ -16,44 +54,26 @@ $(".slider__arrow-right").click(function () {
 
     switch (pictureCounter) {
 
-        case 2:
-            $(".slider__arrow-left").removeClass("slider__arrow--hide");
-            $(".slider__container").css("transform", "translateX(" + translatePercentage + "%)");
-            break;
-        case 3:
-            $(".slider__arrow-right").addClass("slider__arrow--hide");
-            $(".slider__container").css("transform", "translateX(" + translatePercentage + "%)");
-            break;
-    }
-
-    $(".slider__point").removeClass("slider__point--active");
-})
-
-// clic en flecha izquierda -> disminuye
-$(".slider__arrow-left").click(function () {
-
-    let translatePercentage = 0;
-    const percentage = -33;
-
-    pictureCounter--;
-
-    if (pictureCounter === 0) {
-        pictureCounter = 1;
-    }
-
-    translatePercentage = percentage * (pictureCounter - 1);
-
-    switch (pictureCounter) {
-
         case 1:
             $(".slider__arrow-left").addClass("slider__arrow--hide");
             $(".slider__container").css("transform", "translateX(" + translatePercentage + "%)");
+            $(".slider__point").removeClass("slider__point--active");
+            $(".slider__point:nth-of-type(" + pictureCounter + ")").addClass("slider__point--active");
             break;
+
         case 2:
+            $(".slider__arrow-left").removeClass("slider__arrow--hide");
             $(".slider__arrow-right").removeClass("slider__arrow--hide");
             $(".slider__container").css("transform", "translateX(" + translatePercentage + "%)");
+            $(".slider__point").removeClass("slider__point--active");
+            $(".slider__point:nth-of-type(" + pictureCounter + ")").addClass("slider__point--active");
+            break;
+
+        case 3:
+            $(".slider__arrow-right").addClass("slider__arrow--hide");
+            $(".slider__container").css("transform", "translateX(" + translatePercentage + "%)");
+            $(".slider__point").removeClass("slider__point--active");
+            $(".slider__point:nth-of-type(" + pictureCounter + ")").addClass("slider__point--active");
             break;
     }
-
-    $(".slider__point").removeClass("slider__point--active");
-})
+}
