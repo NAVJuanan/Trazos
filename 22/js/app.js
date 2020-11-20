55dd30d73dd28f0a9be032a8a874420d57cb0790
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2020-11-20 18:59:15 +0100

    Clase 030: scss: & / css: slider múltiple

--- 22/js/app.js
+++ 22/js/app.js
@@ -3,19 +3,26 @@ let pictureCounter = 1;
 // clic en flecha derecha -> aumenta
 $(".slider__arrow-right").click(function () {
 
+    let translatePercentage = 0;
+    const percentage = -33;
+
     pictureCounter++;
 
     if (pictureCounter === 4) {
         pictureCounter = 3;
     }
 
+    translatePercentage = percentage * (pictureCounter - 1);
+
     switch (pictureCounter) {
 
         case 2:
             $(".slider__arrow-left").removeClass("slider__arrow--hide");
+            $(".slider__container").css("transform", "translateX(" + translatePercentage + "%)");
             break;
         case 3:
             $(".slider__arrow-right").addClass("slider__arrow--hide");
+            $(".slider__container").css("transform", "translateX(" + translatePercentage + "%)");
             break;
     }
 
@@ -25,19 +32,26 @@ $(".slider__arrow-right").click(function () {
 // clic en flecha izquierda -> disminuye
 $(".slider__arrow-left").click(function () {
 
+    let translatePercentage = 0;
+    const percentage = -33;
+
     pictureCounter--;
 
     if (pictureCounter === 0) {
         pictureCounter = 1;
     }
 
+    translatePercentage = percentage * (pictureCounter - 1);
+
     switch (pictureCounter) {
 
         case 1:
             $(".slider__arrow-left").addClass("slider__arrow--hide");
+            $(".slider__container").css("transform", "translateX(" + translatePercentage + "%)");
             break;
         case 2:
             $(".slider__arrow-right").removeClass("slider__arrow--hide");
+            $(".slider__container").css("transform", "translateX(" + translatePercentage + "%)");
             break;
     }
 
