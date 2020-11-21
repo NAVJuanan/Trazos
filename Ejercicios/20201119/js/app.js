8b54dc6be62f773d4ceb592bdc91de4bb321ebf5
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2020-11-21 22:31:41 +0100

    Clase 032: ejercicios

--- Ejercicios/20201119/js/app.js
+++ Ejercicios/20201119/js/app.js
@@ -5,5 +5,12 @@ $(".cookies__btn-accept").click(function () {
 
 // bxSlider script
 $(document).ready(function () {
-    $('.slider').bxSlider();
+    $(".slider").bxSlider({
+        auto: true,
+        autoControls: true,
+        mode: "horizontal",
+        nextText: ">",
+        prevText: "<",
+        stopAutoOnClick: true,
+    });
 });
