f7404765c12e55e0315e1c42c21f5dae8b48f594
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2020-12-07 13:10:02 +0100

    Clase 037: ejercicios

--- Ejercicios/20201127/js/app.js
+++ Ejercicios/20201127/js/app.js
@@ -9,3 +9,31 @@ $(document).ready(function () {
         stopAutoOnClick: true,
     });
 });
+
+// hover sobre elementos
+$(".main__top1--container1").hover(function () {
+    $(".main__top1--container").addClass("opacity-effect");
+    $(this).removeClass("opacity-effect");
+    $(this).find(".top1__text3").addClass("top1__text3--black");
+}, function () {
+    $(".main__top1--container").removeClass("opacity-effect");
+    $(this).find(".top1__text3").removeClass("top1__text3--black");
+});
+
+$(".main__top1--container2").hover(function () {
+    $(".main__top1--container").addClass("opacity-effect");
+    $(this).removeClass("opacity-effect");
+    $(this).find(".top1__text3").addClass("top1__text3--black");
+}, function () {
+    $(".main__top1--container").removeClass("opacity-effect");
+    $(this).find(".top1__text3").removeClass("top1__text3--black");
+});
+
+$(".main__top1--container3").hover(function () {
+    $(".main__top1--container").addClass("opacity-effect");
+    $(this).removeClass("opacity-effect");
+    $(this).find(".top1__text3").addClass("top1__text3--black");
+}, function () {
+    $(".main__top1--container").removeClass("opacity-effect");
+    $(this).find(".top1__text3").removeClass("top1__text3--black");
+});
