a6d9fd2c581d6e41bc080238fec19238e522cccc
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2021-01-26 14:59:14 +0100

    Clase 065: ejercicios js filtro imágenes

--- Ejercicios/20210125/js/app.js
+++ Ejercicios/20210125/js/app.js
@@ -1,137 +1,59 @@
-// alumnos
-alumnoUno = {
-    firstName: "Javier",
-    lastName: "Pérez Sánchez",
-    birthDate: new Date(2000, 01, 21),
-    grade: 8.5,
-    age: function () {
-        const todayDate = new Date();
-        let currentAge = todayDate.getFullYear() - this.birthDate.getFullYear();
+"use strict";
 
-        // restamos 1 día porque o hay un bug o las horas cuentan para contemplar el mismo día
-        if ((todayDate.getMonth() < this.birthDate.getMonth()) || ((todayDate.getMonth() === this.birthDate.getMonth()) && (todayDate.getDay() < (this.birthDate.getDay() - 1)))) {
-            currentAge--;
-        }
+let nodeAllCards = document.querySelector("#allCards");
+let nodeAllForests = document.querySelector("#allForests");
+let nodeAllSwamps = document.querySelector("#allSwamps");
+let nodeAllMountains = document.querySelector("#allMountains");
 
-        return currentAge;
+// filtro por total
+nodeAllCards.addEventListener("click", function () {
+    let nodeAll = document.querySelectorAll(".filter-image");
+
+    for (let i = 0; i < nodeAll.length; i++) {
+        nodeAll[i].classList.remove("filter-image--hidden");
     }
-}
+})
 
-alumnoDos = {
-    firstName: "María",
-    lastName: "Rodríguez Peña",
-    birthDate: new Date(2000, 10, 21),
-    grade: 9.1,
-    age: function () {
-        const todayDate = new Date();
-        let currentAge = todayDate.getFullYear() - this.birthDate.getFullYear();
+// filtro por cada categoría
 
-        // restamos 1 día porque o hay un bug o las horas cuentan para contemplar el mismo día
-        if ((todayDate.getMonth() < this.birthDate.getMonth()) || ((todayDate.getMonth() === this.birthDate.getMonth()) && (todayDate.getDay() < (this.birthDate.getDay() - 1)))) {
-            currentAge--;
+nodeAllForests.addEventListener("click", function () {
+    let nodeForest = document.querySelectorAll(".filter-image");
+
+    for (let i = 0; i < nodeForest.length; i++) {
+        let nodeClassList = nodeForest[i].className;
+
+        if (!nodeClassList.includes("forest")) {
+            nodeForest[i].classList.add("filter-image--hidden");
+        } else {
+            nodeForest[i].classList.remove("filter-image--hidden");
         }
-
-        return currentAge;
     }
-}
+})
 
-alumnoTres = {
-    firstName: "Pedro",
-    lastName: "Martín Fernández",
-    birthDate: new Date(2000, 03, 12),
-    grade: 7.3,
-    age: function () {
-        const todayDate = new Date();
-        let currentAge = todayDate.getFullYear() - this.birthDate.getFullYear();
+nodeAllSwamps.addEventListener("click", function () {
+    let nodeSwamp = document.querySelectorAll(".filter-image");
 
-        // restamos 1 día porque o hay un bug o las horas cuentan para contemplar el mismo día
-        if ((todayDate.getMonth() < this.birthDate.getMonth()) || ((todayDate.getMonth() === this.birthDate.getMonth()) && (todayDate.getDay() < (this.birthDate.getDay() - 1)))) {
-            currentAge--;
+    for (let i = 0; i < nodeSwamp.length; i++) {
+        let nodeClassList = nodeSwamp[i].className;
+
+        if (!nodeClassList.includes("swamp")) {
+            nodeSwamp[i].classList.add("filter-image--hidden");
+        } else {
+            nodeSwamp[i].classList.remove("filter-image--hidden");
         }
-
-        return currentAge;
     }
-}
+})
 
-alumnoCuatro = {
-    firstName: "Laura",
-    lastName: "Sánchez Pérez",
-    birthDate: new Date(1999, 09, 15),
-    grade: 7.9,
-    age: function () {
-        const todayDate = new Date();
-        let currentAge = todayDate.getFullYear() - this.birthDate.getFullYear();
+nodeAllMountains.addEventListener("click", function () {
+    let nodeMountain = document.querySelectorAll(".filter-image");
 
-        // restamos 1 día porque o hay un bug o las horas cuentan para contemplar el mismo día
-        if ((todayDate.getMonth() < this.birthDate.getMonth()) || ((todayDate.getMonth() === this.birthDate.getMonth()) && (todayDate.getDay() < (this.birthDate.getDay() - 1)))) {
-            currentAge--;
+    for (let i = 0; i < nodeMountain.length; i++) {
+        let nodeClassList = nodeMountain[i].className;
+
+        if (!nodeClassList.includes("mountain")) {
+            nodeMountain[i].classList.add("filter-image--hidden");
+        } else {
+            nodeMountain[i].classList.remove("filter-image--hidden");
         }
-
-        return currentAge;
     }
-}
-
-
-// asignaturas
-asignaturaUno = {
-    name: "Matemáticas",
-    startingDate: new Date(2021, 01, 22),
-    teacher: {
-        firstName: "Julia",
-        lastName: "Machín Hernández",
-        birthDate: new Date(1986, 07, 11),
-        dni: "50123456F",
-        age: function () {
-            const todayDate = new Date();
-            let currentAge = todayDate.getFullYear() - this.birthDate.getFullYear();
-
-            // restamos 1 día porque o hay un bug o las horas cuentan para contemplar el mismo día
-            if ((todayDate.getMonth() < this.birthDate.getMonth()) || ((todayDate.getMonth() === this.birthDate.getMonth()) && (todayDate.getDay() < (this.birthDate.getDay() - 1)))) {
-                currentAge--;
-            }
-
-            return currentAge;
-        }
-    },
-    studentNumber: function () {
-        return this.studentList.length;
-    },
-    studentList: [
-        alumnoUno,
-        alumnoDos,
-        alumnoCuatro
-    ],
-}
-
-
-// mostrar en consola
-console.log(asignaturaUno);
-console.log(asignaturaUno.studentNumber());
-
-console.log(alumnoUno);
-console.log(alumnoUno.age());
-
-console.log(alumnoDos);
-console.log(alumnoDos.age());
-
-console.log(alumnoTres);
-console.log(alumnoTres.age());
-
-console.log(alumnoCuatro);
-console.log(alumnoCuatro.age());
-
-
-// enviar datos a html
-nodeCourseTitle = document.querySelector("#courseTitle")
-nodeCourseTeacher = document.querySelector("#courseTeacher");
-nodeCourseStudents = document.querySelector("#courseStudents");
-
-nodeCourseTitle.textContent = `Curso de ${asignaturaUno.name}`;
-nodeCourseTeacher.textContent = `Profesor: ${asignaturaUno.teacher.firstName} ${asignaturaUno.teacher.lastName}`;
-
-let studentsList = "";
-for (let i = 0; i < asignaturaUno.studentNumber(); i++) {
-    studentsList += `<li>${asignaturaUno.studentList[i].firstName} ${asignaturaUno.studentList[i].lastName}, ${asignaturaUno.studentList[i].age()} años: ${asignaturaUno.studentList[i].grade}</li>`;
-}
-
-nodeCourseStudents.innerHTML = studentsList;
+})
