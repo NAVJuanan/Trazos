6d699597d683f674a469249f303abe3c4dfaec56
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2021-02-08 19:15:09 +0100

    Clase 074: ejercicios js contador textos

--- Ejercicios/20210205-2/js/app.js
+++ Ejercicios/20210205-2/js/app.js
@@ -4,26 +4,24 @@ document.querySelector("#textInput").addEventListener("input", function () {
     const spaceChar = " ";
     const hyphenChar = "-";
     const emptyChar = "";
-    const regex = /[^a-z]/gi;
 
-    // transformamos texto a minúsculas
+    // patrón para excluir letras que no vayan de la 'a' a la 'z' y espacio
+    const regexPattern = /[^a-z ]/g;
+
+    // recogemos y transformamos texto a minúsculas
     const inputValue = this.value.toLowerCase();
-    console.log(inputValue, inputValue.length);
 
-    const regexValue = inputValue.replaceAll(regex, spaceChar);
-    console.log(regexValue);
+    // aplicamos el patrón y sustituimos por una cadena vacía
+    const regexValue = inputValue.replace(regexPattern, emptyChar);
 
     // creamos array por cada palabra separando por espacios
     const inputArray = regexValue.split(spaceChar);
-    console.log(inputArray);
 
-    // filtramos array para descartar espacios vacíos extras tras sustituir caracteres raros
+    // filtramos array para descartar cadenas vacías
     const filteredArray = inputArray.filter(value => value !== emptyChar);
-    console.log(filteredArray);
 
-    // creamos texto a partir de array filtrado separando por guiones
+    // creamos cadena de texto a partir de array filtrado separando por guiones
     const readValue = filteredArray.join(hyphenChar);
-    console.log(readValue);
 
 
     // número de caracteres
@@ -37,4 +35,33 @@ document.querySelector("#textInput").addEventListener("input", function () {
     // texto transformado a slug
     let nodeTextRead = document.querySelector("#textRead");
     nodeTextRead.value = readValue;
-})
+});
+
+// evento para obtener el valor del 'area' tras cada cambio
+document.querySelector("#textLong").addEventListener("input", function () {
+    const spaceChar = " ";
+    const emptyChar = "";
+
+    const inputValue = this.value;
+
+    const inputArray = inputValue.split(spaceChar);
+    const filteredArray = inputArray.filter(value => value !== emptyChar);
+
+    let nodeAreaChars = document.querySelector("#areaChars");
+    nodeAreaChars.innerHTML = `${inputValue.length} caracteres`;
+
+    let nodeAreaWords = document.querySelector("#areaWords");
+    nodeAreaWords.innerHTML = `${filteredArray.length} palabras`;
+});
+
+// evento para convertir a mayúsculas el valor del 'area'
+document.querySelector("#btnUpper").addEventListener("click", function () {
+    let nodeArea = document.querySelector("#textLong");
+    nodeArea.value = nodeArea.value.toUpperCase();
+});
+
+// evento para convertir a minúsculas el valor del 'area'
+document.querySelector("#btnLower").addEventListener("click", function () {
+    let nodeArea = document.querySelector("#textLong");
+    nodeArea.value = nodeArea.value.toLowerCase();
+});
