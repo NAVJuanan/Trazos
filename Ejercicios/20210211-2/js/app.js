2875037d1ce3bf935f0b802131d72880ca6111f4
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2021-02-13 10:51:35 +0100

    Clase 079: ejercicios jquery slider

--- Ejercicios/20210211-2/js/app.js
+++ Ejercicios/20210211-2/js/app.js
@@ -8,17 +8,43 @@ $(document).ready(function () {
     nodeImages.addClass("slider__img--hide");
     nodeImages.first().removeClass("slider__img--hide");
 
+    // variables para controlar tamaño y nodo actual mostrado
+    let nodeCurrent = 0;
+    const nodeSize = nodeImages.length;
+
     // click flecha izquierda
     $(".arrow__left").on({
         click: function () {
-            console.log("izquierda")
+
+            // ocultamos la imagen actual y vamos a la anterior
+            nodeImages.eq(nodeCurrent).addClass("slider__img--hide");
+            nodeCurrent--;
+
+            // control para volver a la última imagen si pasamos el límite
+            if (nodeCurrent < 0) {
+                nodeCurrent = nodeSize - 1;
+            }
+
+            // mostramos la imagen seleccionada
+            nodeImages.eq(nodeCurrent).removeClass("slider__img--hide");
         }
     });
 
     // click flecha derecha
     $(".arrow__right").on({
         click: function () {
-            console.log("derecha")
+
+            // ocultamos la imagen actual y vamos a la siguiente
+            nodeImages.eq(nodeCurrent).addClass("slider__img--hide");
+            nodeCurrent++;
+
+            // control para volver a la primera imagen si pasamos el límite
+            if (nodeCurrent >= nodeSize) {
+                nodeCurrent = 0;
+            }
+
+            // mostramos la imagen seleccionada
+            nodeImages.eq(nodeCurrent).removeClass("slider__img--hide");
         }
     });
 });
