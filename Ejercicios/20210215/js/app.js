2a158a1928400aa13fb96943fd52fdb75018d804
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2021-02-15 20:27:17 +0100

    Clase 080: ejercicios jquery mousemove image

---
+++ Ejercicios/20210215/js/app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*** JQuery ***/
+$(document).ready(() => {
+
+    // mover el ratón por el contenedor de imágenes
+    // $(".image__container").on({
+    //     mousemove: evento => {
+    //         const nodeImage = $(".image__item");
+    //         console.log(evento.pageX, evento.pageY);
+    //         console.log(nodeImage);
+    //     }
+    // });
+});
