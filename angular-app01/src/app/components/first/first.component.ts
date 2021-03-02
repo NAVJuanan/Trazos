fe13cc6e880eddd303453b908591250ef9576e95
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2021-03-02 21:30:24 +0100

    Clase 091: angular -> app.component

---
+++ angular-app01/src/app/components/first/first.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.scss']
+})
+export class FirstComponent implements OnInit {
+
+  // propiedades
+  public title: string = 'first';
+  public name: string = "Juanan";
+  public counter: number = 0;
+
+  // métodos
+  public sumar() {
+    this.counter++;
+  }
+
+  public restar() {
+    this.counter--;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
