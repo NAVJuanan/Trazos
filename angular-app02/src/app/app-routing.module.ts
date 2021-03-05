8cef14291e16153117df5482fcba9e45a5cb6847
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2021-03-05 21:44:16 +0100

    Clase 094: angular -> routing

--- angular-app02/src/app/app-routing.module.ts
+++ angular-app02/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Page1Component } from './components/page1/page1.component';
+import { Page2Component } from './components/page2/page2.component';
+import { Page3Component } from './components/page3/page3.component';
 
-const routes: Routes = [];
+// setup routing
+const routes: Routes = [
+  { path: "", component: Page1Component },
+  { path: "2", component: Page2Component }, // http://localhost:23137/2
+  { path: "3", component: Page3Component } // http://localhost:23137/3
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
