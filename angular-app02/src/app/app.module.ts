1bed521109663aa15a9bc788d795c51aafd419cd
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2021-03-05 17:00:55 +0100

    Clase 094: angular -> routing

---
+++ angular-app02/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigatorComponent } from './components/navigator/navigator.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { Page1Component } from './components/page1/page1.component';
+import { Page2Component } from './components/page2/page2.component';
+import { Page3Component } from './components/page3/page3.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigatorComponent,
+    FooterComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
