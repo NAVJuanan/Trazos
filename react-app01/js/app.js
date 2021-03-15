c6d45ac979459dfaf8818dbdb574a52226538bbd
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2021-03-15 17:59:14 +0100

    Clase 099: react -> new component

--- react-app01/js/app.js
+++ react-app01/js/app.js
@@ -29,18 +29,45 @@ ReactDOM.render(nodeLike(LikeButton), domContainer);
 const nodeHi = React.createElement;
 
 class HolaMundo extends React.Component {
+    label = "h2";
+    obj = {};
+    message = "Hola mundo React";
+
     constructor(props) {
         super(props);
+        this.state = {}; // las propiedades susceptibles de cambiar van aquí
     }
 
     render() {
         return nodeHi(
-            'h2',
-            {},
-            'Hola mundo React'
+            this.label,
+            this.obj,
+            this.message
         );
     }
 }
 
 const domHiContainer = document.querySelector('#hiReact');
 ReactDOM.render(nodeHi(HolaMundo), domHiContainer);
+
+
+// new React component
+const nodeCounter = React.createElement;
+
+class Contador extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { counter: 0 }; // las propiedades susceptibles de cambiar van aquí
+    }
+
+    render() {
+        return nodeHi(
+            'button',
+            { onClick: () => this.setState({ counter: counter + 1 }) },
+            this.counter
+        );
+    }
+}
+
+const domCounter = document.querySelector('#counter');
+ReactDOM.render(nodeCounter(Contador), domCounter);
